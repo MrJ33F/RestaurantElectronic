77ec2663922f0350bd760dd8c20137f4c914b65d
MrJ33F <groza.cosminel@gmail.com>
2022-01-20 09:33:12 +0200

    Rezolvat bug database. :)

--- RestaurantOnline/Migrations/20220120072953_Initial.Designer.cs
+++ RestaurantOnline/Migrations/20220120072953_Initial.Designer.cs
@@ -10,7 +10,7 @@ using RestaurantOnline.Models;
 namespace RestaurantOnline.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20220120065209_Initial")]
+    [Migration("20220120072953_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
