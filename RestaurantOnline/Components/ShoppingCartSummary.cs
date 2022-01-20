4cc3d3da6e26ea21954488a184506d9626b17300
MrJ33F <groza.cosminel@gmail.com>
2022-01-20 09:10:59 +0200

    Rezolvat probleme.

--- RestaurantOnline/Components/ShoppingCartSummary.cs
+++ RestaurantOnline/Components/ShoppingCartSummary.cs
@@ -8,7 +8,27 @@ using RestaurantOnline.Models;
 
 namespace RestaurantOnline.Components
 {
-    public class ShoppingCartSummary
+    public class ShoppingCartSummary : ViewComponent
     {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var ShoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(ShoppingCartViewModel);
+        }
     }
 }
